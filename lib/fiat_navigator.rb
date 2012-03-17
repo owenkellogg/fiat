a1372c4ef83d2c2550d4a16156eb12c921a58892
Steven Zeiler <zeiler.steven@gmail.com>
2012-03-17 18:32:39 -0500

    adjusting spacing on test/test_fiat_navigator.rb.

--- lib/fiat_navigator.rb
+++ lib/fiat_navigator.rb
@@ -1,18 +1,18 @@
-class FiatNavigator
-	DOLLARS_PER_OUNCE_GOLD = 1661.50
+class FiatNavigator 
+  DOLLARS_PER_OUNCE_GOLD = 1661.50
   @@previous_translations = []
 
-  def self.translate_to_gold amount, fiat_money
-		ounces = amount / DOLLARS_PER_OUNCE_GOLD
-	  stash = Stash.new ounces, :gold, :ounces
-		@@previous_translations << "#{amount} #{fiat_money} translates to #{stash.to_s}" 
+  def self.translate_to_gold amount, fiat_money 
+    ounces = amount / DOLLARS_PER_OUNCE_GOLD
+    stash = Stash.new ounces, :gold, :ounces
+    @@previous_translations << "#{amount} #{fiat_money} translates to #{stash.to_s}" 
 
     stash
   end
 
-	def self.do_business_with_the_federal_reserve
-	  "Error: Hopefully this tool will not be used to facilitate such disappointing behavior."	
-	end 
+  def self.do_business_with_the_federal_reserve
+    "Error: Hopefully this tool will not be used to facilitate such disappointing behavior."	
+  end 
 
   def self.previous_translation
     @@previous_translations.first
